9496260b1f5a592ba794172fe4058c9000439625
DejiLaniyan <adedeji.laniyan@gmail.com>
2022-10-15 03:01:21 +0100

    new commit

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedorigins = [
     'https://naijacities.onrender.com',
     'https://naijacities-backend.onrender.com',
+    'http://localhost:3000',
 ]
 
 module.exports = allowedorigins
